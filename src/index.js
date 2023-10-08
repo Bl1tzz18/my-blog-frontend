8ea7a76f0dd674d2a718221818afdfe811350559
Catalin <catalin.filip@lego.com>
2023-10-08 21:55:39 +0200

    Add my-blog-frontend to github

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGL21sYkpDEgW52_XuXfgVWdVwTkX7Llw",
+  authDomain: "my-react-blog-c6f54.firebaseapp.com",
+  projectId: "my-react-blog-c6f54",
+  storageBucket: "my-react-blog-c6f54.appspot.com",
+  messagingSenderId: "296666205058",
+  appId: "1:296666205058:web:09f64197d0a85d70d35d9b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
